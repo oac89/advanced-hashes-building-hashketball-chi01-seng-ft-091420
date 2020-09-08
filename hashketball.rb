fe2898c62deb0788d81ee63f09bef3508327c86b
Oscar A Castro <oacastro2@gmail.com>
2020-09-08 15:32:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,7 +39,7 @@ def game_hash
                           }
                          }
                         
-                      },
+                      }
                           
                         
                         
