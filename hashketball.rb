e02a2ce6c5258406633b242abe3b9e3fd02ddd4b
Oscar A Castro <oacastro2@gmail.com>
2020-09-08 16:04:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,6 @@ def game_hash
                         player_name: "Alan Anderson",
                         number: 0,
                         shoe: 16,
-                        points: 22,
                         rebounds: 12,
                         assists: 12,
                         steals: 3,
