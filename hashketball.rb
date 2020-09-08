550b01bcf192fe320483d60aca9a477d51a11d7b
Oscar A Castro <oacastro2@gmail.com>
2020-09-08 15:27:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,89 @@
 # Write your code here!
+def game_hash
+  {
+    home: {
+      team_name: ("Brooklyn Nets"),
+      colors: ["Black", "White"],
+      players:  [player_name: 
+                        
+        
+                        {"Alan Anderson" =>
+                        {
+                        number: 0,
+                        shoe: 16,
+                        points: 22,
+                        rebounds: 12,
+                        assists: 12,
+                        steals: 3,
+                        blocks: 1,
+                        slam_dunks: 1}
+                          
+                        },
+                        
+                        
+                        
+                        
+        
+                        
+        
+                      {"Reggie Evans" =>   
+                        {
+                        number: 30,
+                        shoe: 14,
+                        points: 12, 
+                        rebounds: 12,
+                        assists: 12,
+                        steals: 12,
+                        blocks: 12,
+                        slam_dunks: 7
+                          }
+                         }
+                        ]
+                      },
+                          
+                        
+                        
+        
+            
+    away: {
+      team_name: ("Charlotte Hornets"),
+      colors: ["Turquoise", "Purple"],
+      players: [player_name: 
+                    
+        
+                       {"Jeff Adrien" =>     
+                        {
+                        number: 4,
+                        shoe: 18,
+                        points: 10, 
+                        rebounds: 1, 
+                        assists: 1,
+                        steals: 2,
+                        blocks: 7,
+                        slam_dunks: 2}
+                         
+                       },
+                          
+                          
+                        
+    
+    
+        
+                      {"Bismack Biyombo" => 
+                          {
+                        number: 0,
+                        shoe: 16,
+                        points: 12,
+                        rebounds: 4,
+                        assists: 7,
+                        steals: 22,
+                        blocks: 15,
+                        slam_dunks: 10} 
+                            
+                          }
+                        
+                      ]
+                    }  
+    }
+end 
+ 
