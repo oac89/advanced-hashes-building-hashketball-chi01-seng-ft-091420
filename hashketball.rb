7581de691fb0edd2833b884ecd8d5b9acb412c18
Oscar A Castro <oacastro2@gmail.com>
2020-09-08 16:47:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,29 +81,29 @@ def game_hash
         steals: 2,
         blocks: 7,
         slam_dunks: 2
-          },
-          {   
-          player_name: "Bismack Biyombo", 
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-          },
-          {
-          player_name: "Bismack Biyombo", 
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-          }
+        },
+      {   
+        player_name: "Bismack Biyombo", 
+        number: 0,
+        shoe: 16,
+        points: 12,
+        rebounds: 4,
+        assists: 7,
+        steals: 22,
+        blocks: 15,
+        slam_dunks: 10
+        },
+        {
+        player_name: "Bismack Biyombo", 
+        number: 0,
+        shoe: 16,
+        points: 12,
+        rebounds: 4,
+        assists: 7,
+        steals: 22,
+        blocks: 15,
+        slam_dunks: 10
+        }
             
               
            
