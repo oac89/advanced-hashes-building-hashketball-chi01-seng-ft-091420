34c4812322f9054b99ab590306335f325eb299c2
Oscar A Castro <oacastro2@gmail.com>
2020-09-08 15:50:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,7 +17,7 @@ def game_hash
                         blocks: 1,
                         slam_dunks: 1
                         
-                      ,
+                      },
                           
                       
                         { 
@@ -58,11 +58,10 @@ def game_hash
                         steals: 2,
                         blocks: 7,
                         slam_dunks: 2
-                      
-                    ,
+                      },
                          
                        
-                          
+                       {   
                         player_name: "Bismack Biyombo", 
                         number: 0,
                         shoe: 16,
